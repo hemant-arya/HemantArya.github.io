0c64d1c5e3fb9b4e42e84023f55cd0ddb695c60f
hemant-arya <aryahemantb@yahoo.co.in>
2022-05-12 05:51:56 +0530

    Add files via upload

---
+++ portfolio-script.js
@@ -0,0 +1,13 @@
+$(document).load(function(){
+	$('.nav-link').click(function(){
+		$('.nav-link').removeClass('active');
+		$(this).addClass('active');
+	})
+});
+
+$(document).ready(function(){
+	$('.nav-link').click(function(){
+		$('.nav-link').removeClass('active');
+		$(this).addClass('active');
+	})
+});
