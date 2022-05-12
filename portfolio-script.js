fe5dd27b3f68c4494158ed1e16465e2f5a813d90
hemant-arya <aryahemantb@yahoo.co.in>
2022-05-12 06:05:55 +0530

    Update portfolio-script.js

--- portfolio-script.js
+++ portfolio-script.js
@@ -1,10 +1,3 @@
-$(document).load(function(){
-	$('.nav-link').click(function(){
-		$('.nav-link').removeClass('active');
-		$(this).addClass('active');
-	})
-});
-
 $(document).ready(function(){
 	$('.nav-link').click(function(){
 		$('.nav-link').removeClass('active');
